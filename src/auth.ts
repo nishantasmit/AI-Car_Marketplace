e28ccf5d7359976d44a64a1846e68338f83ebafa
Nishant Asmit Parida <149187692+nishantasmit@users.noreply.github.com>
2025-08-04 12:27:57 +0530

    auth.ts

---
+++ src/auth.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./lib/prisma";
+ 
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [GitHub],
+  adapter:PrismaAdapter(prisma),
+  session:{
+    strategy:"jwt",
+  },
+});
